017d4fcd03e618fd66d9f59ed4d11405b4919f90
Andreas Urbschat <andur63@outlook.de>
2024-06-01 15:54:54 +0200

    test

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LibreHardwareMonitor;
+
+public class UpdateVisitor : IVisitor
+{
+    public void VisitComputer(IComputer computer)
+    {
+        computer.Traverse(this);
+    }
+    public void VisitHardware(IHardware hardware)
+    {
+        hardware.Update();
+        foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+    }
+    public void VisitSensor(ISensor sensor) { }
+    public void VisitParameter(IParameter parameter) { }
+}
+
+
+namespace ConsoleApp
+{
+
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello");
+            
+
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using LibreHardwareMonitor;
+void Monitor()
+{
+    Computer computer = new Computer
+    {
+        IsCpuEnabled = true,
+        IsGpuEnabled = true,
+        IsMemoryEnabled = true,
+        IsMotherboardEnabled = true,
+        IsControllerEnabled = true,
+        IsNetworkEnabled = true,
+        IsStorageEnabled = true
+    };
+
+    computer.Open();
+    computer.Accept(new UpdateVisitor());
+
+    foreach (IHardware hardware in computer.Hardware)
+    {
+        Console.WriteLine("Hardware: {0}", hardware.Name);
+
+        foreach (IHardware subhardware in hardware.SubHardware)
+        {
+            Console.WriteLine("\tSubhardware: {0}", subhardware.Name);
+
+            foreach (ISensor sensor in subhardware.Sensors)
+            {
+                Console.WriteLine("\t\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+            }
+        }
+
+        foreach (ISensor sensor in hardware.Sensors)
+        {
+            Console.WriteLine("\tSensor: {0}, value: {1}", sensor.Name, sensor.Value);
+        }
+    }
+
+    computer.Close();
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        
+    }
+}
